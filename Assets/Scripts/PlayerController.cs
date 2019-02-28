cf01cc86213994869ccc8b7fdcaf64a054bf738d
Natalia Whiteley <nataliawhiteley11@gmail.com>
2019-02-27 19:42:37 -0500

    Added new scene and fixed some small bugs.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,16 +6,20 @@ public class PlayerController : MonoBehaviour
 {
     public int speed = 20;
     private Rigidbody rigidBody;
+    private Animator anim;
 
     // Start is called before the first frame update
     void Start()
     {
         rigidBody = GetComponent<Rigidbody>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        anim.SetBool("isWalking", false);
+
         float x_pos = Input.GetAxisRaw("Horizontal");
         float z_pos = Input.GetAxisRaw("Vertical");
 
@@ -26,9 +30,10 @@ public class PlayerController : MonoBehaviour
 
         Quaternion new_rot = Quaternion.LookRotation(new_pos, Vector3.up);
         
-        if (new_pos != Vector3.zero)
+        if (new_pos != Vector3.zero) {
+            anim.SetBool("isWalking", true);
             transform.rotation = Quaternion.Slerp(transform.rotation, new_rot, 2.0f);
-
+        }
         transform.position += new_pos * Time.deltaTime * speed;
     }
 }
